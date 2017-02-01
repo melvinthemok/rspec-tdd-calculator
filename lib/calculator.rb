adf60dccc386810be6412cd4e4881729c5fa69f6
melvinthemok <melvinmok.cy@gmail.com>
2017-02-01 14:29:34 +0800

    Complete exercise

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,78 @@
 class Calculator
-  # your class goes here
+
+  def initialize(result)
+    @result = [result]
+  end
+
+  def result
+    @result[0]
+  end
+
+  def reset(number)
+    if @result.length == 1
+      @result = @result.unshift(number)
+    else
+      @result = @result.unshift(number).pop
+      end
+  end
+
+  def add(number)
+    if @result.length == 1
+      @result = @result.unshift(result + number)
+    else
+      @result.unshift(result + number).pop
+      end
+    self
+  end
+
+  def subtract(number)
+    if @result.length == 1
+      @result = @result.unshift(result - number)
+    else
+      @result.unshift(result - number).pop
+      end
+    self
+  end
+
+  def multiply(number)
+    if @result.length == 1
+      @result = @result.unshift(result * number)
+    else
+      @result.unshift(result * number).pop
+      end
+    self
+  end
+
+  def divide(number)
+    if @result.length == 1
+      @result = @result.unshift(result / number)
+    else
+      @result.unshift(result / number).pop
+      end
+    self
+  end
+
+  def operation(operator, number)
+    case operator
+    when 'add'
+      self.add(number)
+    when 'subtract'
+      self.subtract(number)
+    when 'multiply'
+      self.multiply(number)
+    when 'divide'
+      self.divide(number)
+    end
+  end
+
+  def undo
+    temp = @result[0]
+    @result[0] = @result[1]
+    @result[1] = temp
+  end
+
+  def redo
+    undo
+  end
+
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,10 +1,65 @@
 require_relative 'spec_helper'
-require_relative '../lib/item'
+require_relative '../lib/calculator'
 
 describe Calculator do
+
   before(:context) do
-    # initialize calculator
+    @my_calculator = Calculator.new(1)
   end
 
-  # Tests go here
+  describe 'Initialization' do
+    it 'is an instance of the Calculator class' do
+      expect(@my_calculator).to be_instance_of(Calculator)
+    end
+  end
+
+  describe 'Accessors' do
+    it 'should get result' do
+      expect(@my_calculator.result).to eq(1)
+    end
+    it 'should not set result' do
+      @my_calculator.result = 2394 if respond_to? :result=
+      expect(@my_calculator.result).to eq(1)
+    end
+  end
+
+  describe 'Methods' do
+    it 'should reset the result to given number' do
+      @my_calculator.reset(5)
+      expect(@my_calculator.result).to eq(5)
+    end
+    it 'should add a given number to the result' do
+      @my_calculator.add(5)
+      expect(@my_calculator.result).to eq(10)
+    end
+    it 'should subtract a given number from the result' do
+      @my_calculator.subtract(5)
+      expect(@my_calculator.result).to eq(5)
+    end
+    it 'should multiply the result by a given number' do
+      @my_calculator.multiply(2)
+      expect(@my_calculator.result).to eq(10)
+    end
+    it 'should divide the result by a given number' do
+      @my_calculator.divide(2)
+      expect(@my_calculator.result).to eq(5)
+    end
+    it 'should chain add(2) and multiply(2) to get 4' do
+      # @my_calculator.reset(0)
+      expect(@my_calculator.add(2).multiply(2).result).to eq(14)
+    end
+    it 'should take an operation as the first argument and apply it to a given number as the second argument' do
+      @my_calculator.operation('add', 5)
+      expect(@my_calculator.result).to eq(19)
+    end
+    it 'should undo the previous operation, and restore the previous result, which is 14' do
+      @my_calculator.undo
+      expect(@my_calculator.result).to eq(14)
+    end
+    it 'should redo the undone operation, and restore the result, which is 19' do
+      @my_calculator.redo
+      expect(@my_calculator.result).to eq(19)
+    end
+  end
+
 end
